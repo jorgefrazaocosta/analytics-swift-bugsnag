151dec78f718058c02538f9dc889e5e23148c1fc
Jorge Costa <jorge@getzoog.com>
2024-03-28 11:23:58 +0000

    Update to use Segment version 1.5.9

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(
             name: "Segment",
             url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.5.2"
+            from: "1.5.9"
         ),
         .package(
             name: "Bugsnag",
